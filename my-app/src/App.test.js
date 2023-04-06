e7bcf7fb35470c47c80ae468ffce00ad1db15522
Quarks-1 <riop@andrew.cmu.edu>
2023-04-06 16:13:41 -0400

    test added

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,62 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App, { checkWinner } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders game board', () => {
+  const { getAllByRole } = render(<App />);
+  const squares = getAllByRole('button');
+  expect(squares).toHaveLength(9);
+});
+
+test('clicking a square updates the board and switches player', () => {
+  const { getAllByRole, getByText } = render(<App />);
+  const squares = getAllByRole('button');
+  const xSquare = squares[0];
+  const oSquare = squares[1];
+  fireEvent.click(xSquare);
+  expect(xSquare).toHaveTextContent('X');
+  expect(getByText('Next player: O')).toBeInTheDocument();
+  fireEvent.click(oSquare);
+  expect(oSquare).toHaveTextContent('O');
+  expect(getByText('Next player: X')).toBeInTheDocument();
+});
+
+test('detects horizontal win', () => {
+  const board = [
+    'X', 'X', 'X',
+    null, null, null,
+    null, null, null
+  ];
+  const winner = checkWinner(board);
+  expect(winner).toBe('X');
+});
+
+test('detects vertical win', () => {
+  const board = [
+    'O', null, null,
+    'O', null, null,
+    'O', null, null
+  ];
+  const winner = checkWinner(board);
+  expect(winner).toBe('O');
+});
+
+test('detects diagonal win', () => {
+  const board = [
+    null, null, 'X',
+    null, 'X', null,
+    'X', null, null
+  ];
+  const winner = checkWinner(board);
+  expect(winner).toBe('X');
+});
+
+test('detects tie', () => {
+  const board = [
+    'X', 'O', 'X',
+    'O', 'X', 'O',
+    'O', 'X', 'O'
+  ];
+  const winner = checkWinner(board);
+  expect(winner).toBeNull();
 });
