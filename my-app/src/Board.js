3ad902d94abc3cd81f45ac0a10a79e228d48fa96
terryyLi <jielinl@andrew.cmu.edu>
2023-04-06 16:17:38 -0400

    added comments

--- my-app/src/Board.js
+++ my-app/src/Board.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Board.css';
 
+/**
+ * The Board component renders a 3x3 grid of clickable cells, using the provided
+ * `squares` prop to display X's and O's. When a cell is clicked, the `onClick` prop
+ * function is called with the index of the clicked cell.
+ *
+ * @param {object} props - The props object that contains the squares array and onClick function.
+ * @param {string[]} props.squares - An array of 9 strings representing the contents of the game board.
+ * @param {function} props.onClick - A function that is called when a cell is clicked, with the index of the clicked cell.
+ * @returns {JSX.Element} The Board component JSX markup.
+ */
 function Board({ squares, onClick }) {
   return (
     <div className="board">
--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import Board from './Board';
 
+/**
+ * The Game component manages the state of the Tic Tac Toe game and renders the Board and game status.
+ */
 function Game() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const [winner, setWinner] = useState(null);
+  const [board, setBoard] = useState(Array(9).fill(null)); // State for the game board
+  const [xIsNext, setXIsNext] = useState(true); // State for current player
+  const [winner, setWinner] = useState(null); // State for the winner of the game
 
+  /**
+   * Handles the click event for a square on the game board. Updates the board and checks for a winner.
+   * @param {number} index - The index of the clicked square
+   */
   function handleClick(index) {
     const squares = [...board];
 
@@ -26,6 +33,11 @@ function Game() {
     setXIsNext(!xIsNext);
   }
 
+  /**
+   * Determines the winner of the game, if there is one.
+   * @param {Array} squares - The current state of the game board
+   * @returns {string|null} - The symbol of the winner, or null if there is no winner yet
+   */
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -46,6 +58,10 @@ function Game() {
     return null;
   }
 
+  /**
+   * Renders the current game status, either displaying the winner or the next player to move.
+   * @returns {string} - The current game status message
+   */
   function renderStatus() {
     if (winner) {
       return `Winner: ${winner}`;
@@ -53,6 +69,10 @@ function Game() {
     return `Next player: ${xIsNext ? 'X' : 'O'}`;
   }
 
+  /**
+   * Renders the reset button, which resets the game board, player turn, and winner state.
+   * @returns {JSX.Element} - The reset button element
+   */
   function renderResetButton() {
     return (
       <button onClick={() => {
