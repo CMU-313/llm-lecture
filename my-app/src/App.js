49abdb39ea47054cd61123a1874f8a7a1a4dcfe5
Mitun Adenuga <aadenuga@andrew.cmu.edu>
2023-04-06 16:43:42 -0400

    got the game to come up and be pink

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GameBoard from './components/GameBoard';
+// import './app.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Tic Tac Toe </h1>
+      <GameBoard />
     </div>
   );
 }
