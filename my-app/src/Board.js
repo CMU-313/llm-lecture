fe058393eadf71af19826a0fffd72ac31130b1ab
liam-gersten <gerstenliam@gmail.com>
2023-04-06 16:22:43 -0400

    feature 5: draw

--- my-app/src/Board.js
+++ my-app/src/Board.js
@@ -29,6 +29,16 @@ class Board extends Component {
         (board[0] === board[4] && board[4] === board[8] && (board[0] == 'X' || board[0] == 'O')) || 
         (board[2] === board[4] && board[4] === board[6] && (board[2] == 'X' || board[2] == 'O'))) {
       alert(`Player ${this.state.currentPlayer} has won!`);
+    } else if ((board[0] === 'X' || board[0] == 'O') &&
+        (board[1] === 'X' || board[1] == 'O') && 
+        (board[2] === 'X' || board[2] == 'O') &&
+        (board[3] === 'X' || board[3] == 'O') &&
+        (board[4] === 'X' || board[4] == 'O') &&
+        (board[5] === 'X' || board[5] == 'O') &&
+        (board[6] === 'X' || board[6] == 'O') &&
+        (board[7] === 'X' || board[7] == 'O') &&
+        (board[8] === 'X' || board[8] == 'O')) {
+        alert(`Draw!`);
     }
 
   }
