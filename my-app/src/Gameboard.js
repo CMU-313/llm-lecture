ad1a4e676f9ab29df4119a94ba1c51fa532fa5a7
ziyanwang1 <98918127+ziyanwang1@users.noreply.github.com>
2023-04-06 16:19:48 -0400

    player turns

--- my-app/src/Gameboard.js
+++ my-app/src/Gameboard.js
@@ -1,20 +1,41 @@
 import React from "react";
 import "./Gameboard.css";
+import {useState} from "react";
 
 const GameBoard = () => {
-  return (
-    <div className="game-board">
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-    </div>
+
+    const [currentPlayer, setCurrentPlayer] = useState("X");
+
+    const handleCellClick = (event) => {
+        if (event.target.textContent === "") {
+          event.target.textContent = currentPlayer;
+          setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+        }
+    };
+  
+    return (
+        <div>
+        <p>Current player: {currentPlayer}</p>
+        <div className="game-board">
+          <div className="row">
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+          </div>
+          <div className="row">
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+          </div>
+          <div className="row">
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+            <div className="cell" onClick={handleCellClick}></div>
+          </div>
+        </div>
+      </div>
   );
 };
+
 
 export default GameBoard;
