9b759eec82a55d017681a1d1ebcef886ad72ce40
amitag2 <98849921+amitag2@users.noreply.github.com>
2023-04-06 20:22:50 +0000

    finished tic tac toe chatgpt too smart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [board, setBoard] = useState([
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-  ]);
-  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
 
-  const handleClick = (row, col) => {
-    // create a copy of the board
-    const newBoard = [...board];
-    // update the value of the clicked cell
-    newBoard[row][col] = currentPlayer;
-    // toggle the current player
-    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-    // set the new board state
-    setBoard(newBoard);
+  const checkWinner = (board) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winConditions.length; i++) {
+      const [a, b, c] = winConditions[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    if (!board.includes(null)) {
+      return "Draw";
+    }
+    return null;
   };
 
+  const handleClick = (index) => {
+    if (!board[index] && !winner) {
+      const newBoard = [...board];
+      newBoard[index] = player;
+      setBoard(newBoard);
+      const nextPlayer = player === "X" ? "O" : "X";
+      setPlayer(nextPlayer);
+      const result = checkWinner(newBoard);
+      setWinner(result);
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setPlayer("X");
+    setWinner(null);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${player}`;
+  }
+
   return (
-    <div className="App">
-      <h1>3x3 Game Board</h1>
-      <div className="board">
-        <div className="status">Current player: {currentPlayer}</div>
-        {board.map((row, rowIndex) => (
-          <div key={rowIndex} className="row">
-            {row.map((cell, colIndex) => (
-              <div
-                key={colIndex}
-                className="cell"
-                onClick={() => handleClick(rowIndex, colIndex)}
-              >
-                {cell}
-              </div>
-            ))}
-          </div>
-        ))}
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <button onClick={resetGame}>Reset Game</button>
       </div>
     </div>
   );
