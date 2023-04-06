ff8d3b581bf88d6c3d44b25b867a7647378eb51f
zhihengy <zhihengy@andrew.cmu.edu>
2023-04-06 16:07:02 -0400

    basic features implemented

---
+++ tic-tac-toe/src/Game.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  function handleClick(i) {
+    const newBoard = [...board];
+    if (winner || newBoard[i]) return;
+    newBoard[i] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function renderSquare(i) {
+    return (
+      <button className="square" onClick={() => handleClick(i)}>
+        {board[i]}
+      </button>
+    );
+  }
+
+  function renderStatus() {
+    if (winner) {
+      return `Winner: ${winner}`;
+    } else if (board.every(square => square != null)) {
+      return 'Draw!';
+    } else {
+      return `Next player: ${xIsNext ? 'X' : 'O'}`;
+    }
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      <div className="game-info">{renderStatus()}</div>
+    </div>
+  );
+}
+
+export default App;
