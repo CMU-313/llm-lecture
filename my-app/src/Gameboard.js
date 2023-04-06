1e6febbabb6ba08b79db686f1a944b29c6502a18
ziyanwang1 <98918127+ziyanwang1@users.noreply.github.com>
2023-04-06 16:25:15 -0400

    Handle player moves

--- my-app/src/Gameboard.js
+++ my-app/src/Gameboard.js
@@ -1,41 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gameboard.css";
-import {useState} from "react";
+
 
 const GameBoard = () => {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [gameBoard, setGameBoard] = useState([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
 
-    const [currentPlayer, setCurrentPlayer] = useState("X");
+  const handleCellClick = (rowIndex, columnIndex) => {
+    if (gameBoard[rowIndex][columnIndex] === "") {
+      const newGameBoard = [...gameBoard];
+      newGameBoard[rowIndex][columnIndex] = currentPlayer;
+      setGameBoard(newGameBoard);
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    }
+  };
 
-    const handleCellClick = (event) => {
-        if (event.target.textContent === "") {
-          event.target.textContent = currentPlayer;
-          setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-        }
-    };
-  
+  const renderCell = (rowIndex, columnIndex) => {
     return (
-        <div>
-        <p>Current player: {currentPlayer}</p>
-        <div className="game-board">
-          <div className="row">
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-          </div>
-          <div className="row">
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-          </div>
-          <div className="row">
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-            <div className="cell" onClick={handleCellClick}></div>
-          </div>
+      <div
+        className="cell"
+        onClick={() => handleCellClick(rowIndex, columnIndex)}
+      >
+        {gameBoard[rowIndex][columnIndex]}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <p>Current player: {currentPlayer}</p>
+      <div className="game-board">
+        <div className="row">
+          {renderCell(0, 0)}
+          {renderCell(0, 1)}
+          {renderCell(0, 2)}
+        </div>
+        <div className="row">
+          {renderCell(1, 0)}
+          {renderCell(1, 1)}
+          {renderCell(1, 2)}
+        </div>
+        <div className="row">
+          {renderCell(2, 0)}
+          {renderCell(2, 1)}
+          {renderCell(2, 2)}
         </div>
       </div>
+    </div>
   );
 };
-
 
 export default GameBoard;
